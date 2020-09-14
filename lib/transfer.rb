461fada21a8df3df8f5068ffb5c9c2ae07a75ba3
Jeremy Wood <js.wood206@gmail.com>
2020-09-14 18:46:12 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,6 +16,7 @@ class Transfer
 
   def execute_transaction
     if self.valid?
+      @sender.withdraw(@amount)
       @receiver.deposit(@amount)
     else
       "Transaction rejected. Please check your account balance."
